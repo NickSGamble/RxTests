6343ac7c28b7cacd628bcc9b8ac73b9e7898f1d5
g-un-- <gino.ungureanu@cristiman-mbl2.intranet.teamnet.ro>
2013-03-26 13:59:33 +0200

    Test Client-Server speed test

---
+++ RxxSocketTest/RxxSocketTest.Client/Program.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.ComponentModel;
+using System.Configuration;
+using System.Diagnostics;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+using System.Reactive.Concurrency;
+using System.Reactive.Disposables;
+using System.Reactive.Linq;
+using System.Reactive.Subjects;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RxxSocketTest.Client
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            byte[] clientArray = new byte[1024];
+            IPAddress serverAddress = IPAddress.Parse(ConfigurationManager.AppSettings["serverAddress"]);
+            int serverPort = int.Parse(ConfigurationManager.AppSettings["serverPort"]);
+            IPEndPoint serverEndPoint = new IPEndPoint(serverAddress, serverPort);
+
+            var task = GetConnectedClient(serverEndPoint);
+            task.Wait();
+
+            var client =
+                     Observable
+                        .Repeat(clientArray)
+                        .Consume<byte[], int>(array =>
+                            {
+                                return task.Result.SendToObservable(array, 0, 1024, SocketFlags.None, serverEndPoint);
+                            });
+                            
+            Subject<long> totalClient = new Subject<long>();
+            Stopwatch stopWatch = new Stopwatch();
+
+            stopWatch.Start();
+            var clientDisposable =
+                client.SubscribeOn(ThreadPoolScheduler.Instance)
+                .ObserveOn(Scheduler.Immediate)
+                .Subscribe(x =>
+                    {
+                        if (x > 0) totalClient.OnNext(x);
+                        else totalClient.OnCompleted();
+                    });
+
+            var oneMegabit = 1048576;
+
+            totalClient
+                .SubscribeOn(ThreadPoolScheduler.Instance)
+                .Window(TimeSpan.FromSeconds(1))
+                .Subscribe(x =>
+                {
+                    x.SubscribeOn(ThreadPoolScheduler.Instance).Aggregate((seed, incr) => seed + incr)
+                        .Subscribe(sum => Console.WriteLine("client speed: {0}", sum / oneMegabit));
+                });
+
+            Console.ReadLine();
+
+            stopWatch.Stop();
+            clientDisposable.Dispose();
+
+            Console.ReadLine();
+        }
+
+        private static async Task<Socket> GetConnectedClient(IPEndPoint serverEndPoint)
+        {
+            IObservable<Socket> clientSocket =
+               from socket in ObservableSocket.Connect(
+                   AddressFamily.InterNetwork,
+                   SocketType.Stream,
+                   ProtocolType.Tcp,
+                   serverEndPoint)
+               select socket;
+
+            var connectedClient = await clientSocket.FirstAsync();
+            return connectedClient;
+        }
+    }
+}
---
+++ RxxSocketTest/RxxSocketTest/Program.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.ComponentModel;
+using System.Configuration;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Reactive.Concurrency;
+using System.Reactive.Disposables;
+using System.Reactive.Linq;
+using System.Reactive.Subjects;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RxxSocketTest
+{
+    public class SocketLab
+    {
+        public static void Main()
+        {
+            byte[] serverArray = new byte[1024];
+            IPAddress serverAddress = IPAddress.Parse(ConfigurationManager.AppSettings["serverAddress"]);
+            int serverPort = int.Parse(ConfigurationManager.AppSettings["serverPort"]);
+            IPEndPoint serverEndPoint = new IPEndPoint(serverAddress, serverPort);
+
+            var task = GetListener(serverEndPoint);
+            task.Wait();
+
+            var server = Observable.Repeat(serverArray)
+                         .Consume<byte[], int>(array =>
+                             {
+                                 return task.Result.ReceiveObservable(serverArray, 0, 1024, SocketFlags.None);
+                             });
+
+            Subject<long> totalServer = new Subject<long>(); 
+
+            Stopwatch stopWatch = new Stopwatch();
+            stopWatch.Start();
+
+            var serverDisposable =
+                server.SubscribeOn(ThreadPoolScheduler.Instance)
+                .ObserveOn(Scheduler.Immediate)
+                .Subscribe(x =>
+                {
+                    if (x > 0) totalServer.OnNext(x);
+                    else totalServer.OnCompleted();
+                });
+            
+            var oneMegabit = 1048576;
+
+            totalServer
+                .SubscribeOn(NewThreadScheduler.Default)
+                .Window(TimeSpan.FromSeconds(1))
+                .Subscribe(x =>
+                    {
+                        x.SubscribeOn(ThreadPoolScheduler.Instance).Aggregate((seed, incr) => seed + incr)
+                            .Subscribe(sum => Console.WriteLine("server speed: {0}", sum/oneMegabit));
+                    });
+
+            Console.ReadLine();
+
+            stopWatch.Stop();
+            serverDisposable.Dispose();
+
+            Console.ReadLine();
+        }
+
+        private static async Task<Socket> GetListener(IPEndPoint serverEndPoint)
+        {
+            IObservable<Socket> serverSocket =
+                from socket in ObservableSocket.Accept(
+                    AddressFamily.InterNetwork,
+                    SocketType.Stream,
+                    ProtocolType.Tcp,
+                    serverEndPoint,
+                    count: 1).Do(_ => Console.WriteLine("server created"))
+                select socket;
+
+            var listener = await serverSocket.FirstAsync();
+            return listener;
+        }
+    }
+}
