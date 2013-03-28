45f823d5acedc5eb7478d27f92a9dafaa0fde90d
g-un-- <gino.ungureanu@cristiman-mbl2.intranet.teamnet.ro>
2013-03-28 15:49:50 +0200

    Added first QbservableTcpProvider

---
+++ RxxQbservableTest/RxxQbservableTest.Client/Program.cs
@@ -0,0 +1,28 @@
+﻿using QbservableProvider;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Reactive.Concurrency;
+using System.Reactive.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RxxQbservableTest.Client
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var client = new QbservableTcpClient<long>(new IPEndPoint(IPAddress.Parse("10.6.40.209"), 3205));
+            
+            using (client.Query().Subscribe(
+                value => Console.WriteLine("Client observed: " + value),
+                ex => Console.WriteLine("Error: {0}", ex.Message),
+                () => Console.WriteLine("Completed")))
+            {
+                Console.ReadLine();
+            }
+        }
+    }
+}
---
+++ RxxQbservableTest/RxxQbservableTest/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Reactive.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Reactive;
+using Rxx;
+using Rxx.Parsers;
+using Rxx.Properties;
+using Rxx.Parsers.Linq;
+using Rxx.Parsers.Reactive;
+using Rxx.Parsers.Reactive.Linq;
+using QbservableProvider;
+
+namespace RxxQbservableTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IObservable<long> source = Observable.Interval(TimeSpan.FromSeconds(1));
+
+            var service = source.ServeQbservableTcp(
+                new IPEndPoint(IPAddress.Parse("10.6.40.209"), 3205));
+
+            using (service.Subscribe(
+                client => Console.WriteLine("Client shutdown."),
+                ex => Console.WriteLine("Error: {0}", ex.Message),
+                () => Console.WriteLine("This will never be printed because a service host never completes.")))
+            {
+                Console.ReadLine();
+            }
+        }
+    }
+}
